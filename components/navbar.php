c885dd62fd8ec6d281ef3ed734ff54e0ca982ff6
AagieKLZ <darkyelan@gmail.com>
2023-03-03 13:05:06 +0100

    Added animation for open dropdown

--- components/navbar.php
+++ components/navbar.php
@@ -1,34 +1,21 @@
-<nav class="navbar navbar-expand-lg bg-primary-subtle ">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <img alt="tenfe" src="assets/logo.png" style="height: 65px"></img>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse d-flex-lg justify-content-end container-fluid" id="navbarNav">
-            <ul class="navbar-nav row gx-5">
-                <li class="nav-item col">
-                    <a class="nav-link text-center fw-bold px-5 underline-on-hover" href="#">Rutas</a>
-                </li>
-                <li class="nav-item col">
-                    <a class="nav-link text-center fw-bold px-5 underline-on-hover" href="#">Horarios</a>
-                </li>
-                <li class="nav-item col">
-                    <a class="nav-link text-center fw-bold px-5 underline-on-hover" href="#">Viajeros</a>
-                </li>
-                <?php 
-                if (isset($_SESSION['user'])) {
-                    echo '<li class="nav-item col lg-span-4">
-                    <a href="login.php" class="btn btn-success btn-sm text-center px-5 mt-1 text-nowrap fw-bold">Cerrar Sesión</a>
-                </li>';
-                } else {
-                    echo '<li class="nav-item col lg-span-4">
-                    <a href="login.php" class="btn btn-success btn-sm text-center px-5 mt-1 text-nowrap fw-bold">Iniciar Sesión</a></li>';
-                }
-                ?>
-            </ul>
-
-        </div>
+<nav class="lg:w-[90%] lg:left-[5%] left-0 w-full h-14 border-b border-b-black absolute top-0 left-0 flex flex-row justify-around items-center">
+    <img src="assets/logo.png" alt="logo" class="h-14 my-auto">
+    <div class="lg:flex hidden justify-end space-x-16 items-center">
+        <a href="#" class="hover:underline text-lg text-fuchsia-900">Rutas</a>
+        <a href="#" class="hover:underline text-lg text-fuchsia-900">Horarios</a>
+        <a href="#" class="hover:underline text-lg text-fuchsia-900">Billetes</a>
     </div>
+    <a href="#" class="btn lg:flex hidden py-2 px-3 font-semibold hover:bg-fuchsia-900 rounded-lg text-fuchsia-900 flex items-center border border-fuchsia-900">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 fill-fuchsia-900 mr-2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+        </svg>
+        <span>Iniciar Sesión</span>
+    </a>
+    <input type="checkbox" name="dropdown" id="menu-dropdown" class="hidden">
+    <label id="menu-checkbox" for="menu-dropdown" class="lg:hidden flex justify-center items-center p-8 h-full">
+        <div id="topbar" class="absolute top-4 w-8 bg-black rounded-full h-[2px]"></div>
+        <div id="middlebar" class="absolute top-6 w-8 bg-black rounded-full h-[2px]"></div>
+        <div id="botbar" class="absolute top-8 w-8 bg-black rounded-full h-[2px]"></div>
+    </label>
+    
 </nav>
