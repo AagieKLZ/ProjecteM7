e889909ddca817f60139445b6d99ee6542a3d11a
AagieKLZ <darkyelan@gmail.com>
2023-03-03 10:14:51 +0100

    Responsive navbar fixed

--- components/navbar.php
+++ components/navbar.php
@@ -6,7 +6,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse d-flex justify-content-end container-fluid" id="navbarNav">
+        <div class="collapse navbar-collapse d-flex-lg justify-content-end container-fluid" id="navbarNav">
             <ul class="navbar-nav row gx-5">
                 <li class="nav-item col">
                     <a class="nav-link text-center fw-bold px-5 underline-on-hover" href="#">Rutas</a>
@@ -19,11 +19,11 @@
                 </li>
                 <?php 
                 if (isset($_SESSION['user'])) {
-                    echo '<li class="nav-item col span-4">
+                    echo '<li class="nav-item col lg-span-4">
                     <a href="login.php" class="btn btn-success btn-sm text-center px-5 mt-1 text-nowrap fw-bold">Cerrar Sesión</a>
                 </li>';
                 } else {
-                    echo '<li class="nav-item col span-4">
+                    echo '<li class="nav-item col lg-span-4">
                     <a href="login.php" class="btn btn-success btn-sm text-center px-5 mt-1 text-nowrap fw-bold">Iniciar Sesión</a></li>';
                 }
                 ?>
