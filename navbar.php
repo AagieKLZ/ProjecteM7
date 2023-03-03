3be53ba8473a9d4f870f484f384c66c844ee1979
AagieKLZ <darkyelan@gmail.com>
2023-03-03 09:22:38 +0100

    Added navbar

---
+++ navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <h2 class="text-light">Tenfe</h2>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav row gx-5">
+                <li class="nav-item col">
+                    <a class="nav-link text-light text-center" href="#">Home</a>
+                </li>
+                <li class="nav-item col">
+                    <a class="nav-link text-light text-center" href="#">Rutas</a>
+                </li>
+                <li class="nav-item col">
+                    <a class="nav-link text-light text-center" href="#">Horarios</a>
+                </li>
+                <li class="nav-item col">
+                    <a class="nav-link text-light text-center">Viajeros</a>
+                </li>
+                <li class="nav-item col span-4">
+                    <button class="btn btn-success btn-sm text-center px-5 text-nowrap">Iniciar Sesión</button>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
