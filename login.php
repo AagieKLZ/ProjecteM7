367ec5834d39bb9524b88776effbf7452e82b918
AagieKLZ <darkyelan@gmail.com>
2023-03-09 14:15:03 +0100

    Added required fields and extra responsivity

--- login.php
+++ login.php
@@ -20,12 +20,12 @@
     <?php include 'components/background.php'; ?>
     <main class="flex flex-row justify-center items-center w-full h-[calc(100%-3.5rem)] absolute top-14 bg-gradient from-white to-gray-200">
         
-    <form action="./lib/login.php" method="POST" class="lg:w-1/3 md:w-1/2 w-2/3 h-fit py-24 flex flex-col justify-center items-center bg-gray-50 bg-opacity-50 backdrop-blur space-y-4 rounded-3xl drop-shadow-xl">
+    <form action="./lib/login.php" method="POST" class="lg:w-1/3 md:w-1/2 sm:w-2/3 w-[90%] h-fit py-24 flex flex-col justify-center items-center bg-gray-50 bg-opacity-50 backdrop-blur space-y-4 rounded-3xl drop-shadow-xl">
             <img src="./assets/logo.png" alt="Logo" class="w-1/3 -mt-16">
             <h1 class="text-xl font-semibold text-fuchsia-900">Acceso para administradores</h1>
             <div class="w-full flex flex-col justify-center items-center">
-                <label for="username" class="block flex w-2/3 justify-start text-fuchsia-900 font-semibold">Email</label>
-                <input type="email" name="username" id="username" class="w-2/3 h-10 text-fuchsia-900 font-light border-2 border-fuchsia-800 rounded-lg px-2">
+                <label for="username" class="block flex md:w-2/3 w-[90%] justify-start text-fuchsia-900 font-semibold">Email</label>
+                <input required type="email" name="username" id="username" class="md:w-2/3 w-[90%] h-10 text-fuchsia-900 font-light border-2 border-fuchsia-800 rounded-lg px-2">
             </div>
             <?php 
                 if (isset($_SESSION["errorCode"]) && $_SESSION["errorCode"] == "404"){
@@ -33,11 +33,11 @@
                 }
             ?>
             <div class="w-full flex flex-col justify-center items-center">
-                <label for="password" class="block flex w-2/3 justify-start text-fuchsia-900 font-semibold">Contraseña</label>
-                <input type="password" name="password" id="password" class="w-2/3 text-fuchsia-900 h-10 border-2 border-fuchsia-800 rounded-lg px-2">
+                <label for="password" class="block flex md:w-2/3 w-[90%] justify-start text-fuchsia-900 font-semibold">Contraseña</label>
+                <input required type="password" name="password" id="password" class="md:w-2/3 w-[90%] text-fuchsia-900 h-10 border-2 border-fuchsia-800 rounded-lg px-2">
             </div>
-            <a href="#" class="block text-fuchsia-900 underline w-2/3 flex flex-row justify-start items-center font-light">¿Has olvidado tu contraseña?</a>
-            <button class="w-2/3 py-2 border-2 border-fuchsia-900 rounded-lg text-fuchsia-900 font-semibold hover:text-white hover:bg-fuchsia-900">Iniciar Sesión</button>
+            <a href="#" class="block text-fuchsia-900 underline md:w-2/3 w-[90%] flex flex-row justify-start items-center font-light">¿Has olvidado tu contraseña?</a>
+            <button class="md:w-2/3 w-[90%] py-2 border-2 border-fuchsia-900 rounded-lg text-fuchsia-900 font-semibold hover:text-white hover:bg-fuchsia-900">Iniciar Sesión</button>
         </form>
     </main>
 
