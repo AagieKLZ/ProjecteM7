4ef9a3906f44b7ec41843b25652dfee12ccc96ca
AagieKLZ <darkyelan@gmail.com>
2023-03-09 12:09:11 +0100

    Added buttons for admin login

---
+++ components/loginButton.php
@@ -0,0 +1,6 @@
+<a href="login.php" class="btn lg:flex hidden py-2 px-3 font-semibold hover:bg-fuchsia-900 rounded-lg text-fuchsia-900 flex items-center border border-fuchsia-900">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 fill-fuchsia-900 mr-2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+        </svg>
+        <span>Iniciar Sesión</span>
+    </a>
