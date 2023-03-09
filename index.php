ee2fe90a4b9c624f39f9c7bc875749703f2dba90
AagieKLZ <darkyelan@gmail.com>
2023-03-09 14:15:13 +0100

    Changed input boxes

--- index.php
+++ index.php
@@ -25,9 +25,9 @@ session_get_cookie_params();
     <main class="flex flex-row justify-center items-center w-full h-[calc(100%-3.5rem)] mt-[5rem]">
         <div class="w-full h-fit flex flex-col items-center justify-center">
             <div class="w-full flex items-center justify-center -mt-12"><img src="./assets/logo.png" class="w-[170px]"></div>
-            <div class="w-[90%] rounded-lg h-fit bg-fuchsia-100 bg-opacity-50 backdrop-blur p-8 py-12 border-2 border-fuchsia-400 drop-shadow-xl">
+            <div class="w-[90%] rounded-lg h-fit bg-fuchsia-100 bg-opacity-25 backdrop-blur p-8 py-12 drop-shadow-xl">
                 <h1 class="font-semibold text-2xl">Consulta de horarios</h1>
-                <form class="flex lg:flex-row flex-col min-w-fit justify-around items-end lg:space-y-0 space-y-2 w-full mt-8">
+                <form class="flex lg:flex-row flex-col min-w-fit justify-around lg:items-end items-center lg:space-y-0 space-y-2 w-full mt-8">
                     <div class="flex flex-col space-y-1">
                         <label for="origin" class="font-semibold">Origen</label>
                         <select name="origin" id="origin" placeholder="----" class="w-44 bg-white p-2 rounded-lg border-2 border-fuchsia-900">
@@ -56,7 +56,7 @@ session_get_cookie_params();
                             <option value="">15:00</option>
                         </select>
                     </div>
-                    <button class="bg-fuchsia-100 hover:bg-fuchsia-900 hover:text-white text-fuchsia-900 border-2 font-semibold border-fuchsia-900 w-fit px-10 py-2 rounded-lg flex justify-between items-center text-xl">
+                    <button class="bg-gray-200 bg-opacity-75 hover:bg-fuchsia-900 hover:text-white text-black border-2 font-semibold border-black w-fit px-10 py-2 rounded-lg flex justify-between items-center md:text-xl text-lg">
                         Buscar
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-6 h-6 ml-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -70,13 +70,13 @@ session_get_cookie_params();
                     <div class="lg:w-[175px] lg:h-[175px] w-[100px] h-[100px] drop-shadow-xl bg-emerald-500 hover:bg-emerald-600 rounded-full flex justify-center items-center">
                         <img src="assets/train.svg" class="lg:h-[75px] h-[50px]" />
                     </div>
-                    <div class="text-2xl mt-2 font-semibold">Líneas</div>
+                    <div class="lg:text-2xl text-lg mt-2 font-semibold">Líneas</div>
                 </a>
                 <a href="#" class="flex flex-col justify-center items-center hover:font-semibold hover:scale-105">
                     <div class="lg:w-[175px] lg:h-[175px] w-[100px] h-[100px] drop-shadow-xl bg-emerald-500 hover:bg-emerald-600 rounded-full flex justify-center items-center">
                         <img src="assets/time.svg" class="lg:h-[75px] h-[50px]" />
                     </div>
-                    <div class="text-2xl mt-2 font-semibold">Horarios</div>
+                    <div class="lg:text-2xl text-lg mt-2 font-semibold">Horarios</div>
                 </a>
                 <a href="#" class="flex flex-col justify-center items-center hover:font-semibold hover:scale-105">
                     <div class="lg:w-[175px] lg:h-[175px] w-[100px] h-[100px] drop-shadow-xl bg-emerald-500 rounded-full hover:bg-emerald-600 flex justify-center items-center">
@@ -85,7 +85,7 @@ session_get_cookie_params();
                         </svg>
 
                     </div>
-                    <div class="text-2xl mt-2 font-semibold">Estaciones</div>
+                    <div class="lg:text-2xl text-lg mt-2 font-semibold">Estaciones</div>
                 </a>
             </div>
         </div>
