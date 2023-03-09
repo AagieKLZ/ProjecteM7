a8d9d785178a72ab6f522cd594b8fb420a35047a
AagieKLZ <darkyelan@gmail.com>
2023-03-09 14:04:48 +0100

    Set background image

--- components/navbar.php
+++ components/navbar.php
@@ -1,9 +1,9 @@
-<nav class="lg:w-[90%] lg:left-[5%] left-0 w-full h-14 border-b border-b-black absolute top-0 left-0 flex flex-row justify-around items-center">
+<nav class="fixed lg:w-full left-0 w-full h-14 border-b border-b-black bg-transparent backdrop-blur z-50 top-0 left-0 flex flex-row justify-around items-center">
     <a href="index.php" class="block h-14 my-auto"><img src="assets/logo.png" alt="logo" class="h-14 my-auto"></a>
     <div class="lg:flex hidden justify-end space-x-16 items-center">
-        <a href="#" class="hover:underline text-lg text-fuchsia-900">Rutas</a>
+        <a href="./routes.php" class="hover:underline text-lg text-fuchsia-900">Líneas</a>
         <a href="#" class="hover:underline text-lg text-fuchsia-900">Horarios</a>
-        <a href="#" class="hover:underline text-lg text-fuchsia-900">Billetes</a>
+        <a href="./stations.php" class="hover:underline text-lg text-fuchsia-900">Estaciones</a>
     </div>
     <!-- <?php include "components/loginButton.php"; ?> -->
     <?php 
