d40b1ef95d92c58a7325a835c4f841f6bcac5876
abru <albertriudomenc05@gmail.com>
2023-03-10 08:50:03 +0100

    Optimized getRoutesByStationId

--- api/trainRoutes.php
+++ api/trainRoutes.php
@@ -11,7 +11,7 @@ class trainRoutes
      * @param $trainNumber int train number
      * @return array with route name, destination station id and name, time, each row is a stop,
      */
-    static function getRouteByTrainNumber($trainNumber)
+    static function getRouteByTrainNumber(int $trainNumber): array
     {
         $db = new dbClient();
         $sql = "SELECT route_id, station_id,  name, time, stop_number
@@ -28,37 +28,25 @@ class trainRoutes
      * @param $stationId int station id
      * @return array with all routes that pass through the station, each row is a route [route, time it passes, train number, origin, destination]
      */
-    static function getRoutesByStationId($stationId)
+    static function getRoutesByStationId(int $stationId, string $time = null): array
     {
+        if ($time == null) {
+            $time = date("H:i:s");
+        }
         $db = new dbClient();
         $sql = "SELECT route_id, time, train_num FROM schedules
-        WHERE station_id = ?;";
-        $params = [$stationId];
+        WHERE station_id = ?
+        AND time > ?
+        ORDER BY time
+        LIMIT 10;";
+        $params = [$stationId, $time];
         $routes = $db->query($sql, $params);
         // Maps routes and add origin and destination
-        $routes = array_map(function ($route) {
+        return array_map(function ($route) {
             $route["origin"] = (new trainRoutes)->getRouteOrigin($route["train_num"]);
             $route["destination"] = (new trainRoutes)->getRouteDestination($route["train_num"]);
             return $route;
         }, $routes);
-        // Ordenamos las rutas por hora de mas temprano a mas tarde
-        usort($routes, function ($a, $b) {
-            return $a["time"] <=> $b["time"];
-        });
-        return $routes;
-    }
-
-    /**
-     * Gets the name of a station, given a station id
-     * @param $stationId int station id
-     * @return array with station name
-     */
-    static function getStationName($stationId)
-    {
-        $db = new dbClient();
-        $sql = "SELECT name FROM stations WHERE id = ?;";
-        $params = [$stationId];
-        return $db->query($sql, $params);
     }
 
     /**
@@ -66,7 +54,7 @@ class trainRoutes
      * @param $trainNumber int train number
      * @return array with route name, origin station id and name, time
      */
-    private function getRouteOrigin($trainNumber)
+    private function getRouteOrigin(int $trainNumber): array
     {
         $db = new dbClient();
         $sql = "SELECT route_id, s.id, s.name, time FROM schedules
@@ -82,7 +70,7 @@ class trainRoutes
      * @param $trainNumber int train number
      * @return array with route name, destination station id and name, time
      */
-    private function getRouteDestination($trainNumber)
+    private function getRouteDestination(int $trainNumber): array
     {
         $db = new dbClient();
         $sql = "SELECT route_id, s.id, s.name, time FROM schedules
@@ -91,6 +79,34 @@ class trainRoutes
         ORDER BY stop_number DESC LIMIT 1";
         $params = [$trainNumber];
         return $db->query($sql, $params);
+    }
+
+    /**
+     * Gets the name of a station, given a station id
+     * @param $stationId int station id
+     * @return array with station name
+     */
+    static function getStationName(int $stationId): array
+    {
+        $db = new dbClient();
+        $sql = "SELECT name FROM stations WHERE id = ?;";
+        $params = [$stationId];
+        return $db->query($sql, $params);
+    }
+
+    /**
+     * Gets all the stations and their ids
+     * @return array with all stations and their ids
+     */
+    static function getAllStations(): array
+    {
+        $db = new dbClient();
+        $sql = "SELECT id, name FROM stations;";
+        $stations = $db->query($sql,[]);
+        usort($stations, function ($a, $b) {
+            return $a["name"] <=> $b["name"];
+        });
+        return $stations;
     }
 
 
