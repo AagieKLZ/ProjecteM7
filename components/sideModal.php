3b0857f4aaaf9462f0cac351354fcf84f14da58b
AagieKLZ <darkyelan@gmail.com>
2023-03-10 09:15:01 +0100

    Close modal

--- components/sideModal.php
+++ components/sideModal.php
@@ -1,4 +1,4 @@
-<div class="modal fixed flex justify-end inset-0 w-full h-screen bg-gray-100 bg-opacity-25 backdrop-blur-xl z-40">
+<div class="modal lg:hidden fixed flex justify-end inset-0 w-full h-screen bg-gray-100 bg-opacity-25 backdrop-blur-xl z-40">
     <div class="sm:w-1/2 w-[90%] h-screen bg-gray-200 flex flex-col justify-start items-end space-y-6 pt-24 px-4">
     <a href="./routes.php" class="text-xl text-fuchsia-900 menu-item">Líneas</a>
         <a href="#" class="text-xl text-fuchsia-900 menu-item">Horarios</a>
