f13fb74258977f718483f599c4cc4a322d5e7a34
AagieKLZ <darkyelan@gmail.com>
2023-03-17 11:52:29 +0100

    Finished admin landing page

--- admin.php
+++ admin.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 session_get_cookie_params();
+if (!isset($_SESSION['user'])) {
+    header('Location: ./index.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,46 +21,35 @@ session_get_cookie_params();
 <body class="mt-5">
 
     <?php include 'components/navbar.php'; ?>
-    <main class="flex flex-col justify-center items-center w-full h-[calc(100%-3.5rem)] mt-[3.5rem]">
-        <div class="mt-4 text-3xl font-semibold">Panel de Administración</div>
-        <div class="md:w-2/3 w-[90%] mt-12 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12">
-            <div class="border-2 border-fuchsia-700 h-48 rounded-2xl text-fuchsia-700">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-10 h-10 relative inset-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
-                </svg>
-                <div class="relative bottom-0 text-right mt-28 mr-4 text-xl font-semibold">Añadir Líneas</div>
-            </div>
-            <div class="border-2 border-blue-600 h-48 rounded-2xl text-blue-600">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-10 h-10 relative inset-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-                </svg>
-                <div class="relative bottom-0 text-right mt-28 mr-4 text-xl font-semibold">Añadir Horarios</div>
-            </div>
-            <div class="border-2 border-emerald-600 h-48 rounded-2xl text-emerald-600">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-10 h-10 relative inset-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-                </svg>
-                <div class="relative bottom-0 text-right mt-28 mr-4 text-xl font-semibold">Añadir Usuarios</div>
-            </div>
-            <div class="border-2 border-fuchsia-700 h-48 rounded-2xl text-fuchsia-700">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-10 h-10 relative inset-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
-                </svg>
-                <div class="relative bottom-0 text-right mt-28 mr-4 text-xl font-semibold">Modificar Líneas</div>
-            </div>
-            <div class="border-2 border-blue-600 h-48 rounded-2xl text-blue-600">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-10 h-10 relative inset-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-                </svg>
-                <div class="relative bottom-0 text-right mt-28 mr-4 text-xl font-semibold">Modificar Horarios</div>
-            </div>
-            <div class="border-2 border-emerald-600 h-48 rounded-2xl text-emerald-600">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 relative inset-4">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-</svg>
+    <?php include 'components/adminBg.php'; ?>
 
-                <div class="relative bottom-0 text-right mt-28 mr-4 text-xl font-semibold">Modificar Usuarios</div>
-            </div>
+    <main class="flex flex-col justify-center items-center w-full h-[calc(100%-3.5rem)] mt-[3.5rem]">
+        <div class="mb-8 mt-16 text-3xl font-semibold">Panel de Administración</div>
+        <div class="md:w-2/3 w-[90%] h-[50vh] flex md:flex-row flex-col justify-center items-center gap-12 mt-24">
+            <a href="./schedule/view.php" id="schedule-card" class="card block grid grid-rows-2 bg-gray-100 bg-opacity-50 hover:bg-fuchsia-100 hover:bg-opacity-25 drop-shadow-xl backdrop-blur grid-cols-2 items-end border-2 border-blue-600 h-full w-2/3 rounded-2xl text-blue-600">
+                <div class="w-full h-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-12 h-12 relative inset-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                    </svg>
+                </div>
+                <div></div>
+                <div class="lg:block hidden"></div>
+                <div class="text-right p-4 md:text-2xl text-xl font-semibold">Administrar Horarios</div>
+                <div class="lg:hidden block"></div>
+            </a>
+            <a href="./users.php" id="user-card" class="card block grid grid-rows-2 grid-cols-2 bg-gray-100 hover:bg-green-100 hover:bg-opacity-25 bg-opacity-50 drop-shadow-xl backdrop-blur items-end border-2 border-emerald-600 h-full w-2/3 rounded-2xl text-emerald-600">
+                <div class="w-full h-full">
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 relative inset-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                    </svg>
+
+                </div>
+                <div></div>
+                <div class="lg:block hidden"></div>
+                <div class="text-right p-4 md:text-2xl text-xl font-semibold">Administrar Usuarios</div>
+                <div class="lg:hidden block"></div>
+            </a>
         </div>
     </main>
 </body>
