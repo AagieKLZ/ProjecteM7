e7b0930002908250f1e3fb43f5c90ef4f408dc0f
AagieKLZ <darkyelan@gmail.com>
2023-03-23 14:02:41 +0100

    Panel for schedule administration

--- schedule/view.php
+++ schedule/view.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 session_get_cookie_params();
-if (!isset($_SESSION['user'])) { header('Location: ../index.php'); }
+if (!isset($_SESSION['user'])) {
+    header('Location: ../index.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,23 +22,63 @@ if (!isset($_SESSION['user'])) { header('Location: ../index.php'); }
 <body class="mt-5">
     <?php include '../components/altNavbar.php' ?>
     <main class="flex flex-col justify-center items-center w-full h-[calc(100%-3.5rem)] mt-[3.5rem]">
-        <div class="mb-8 mt-16 text-3xl font-semibold">Administración de Horarios</div>
+        <div class="mt-16 mb-8 text-3xl font-semibold">Administración de Horarios</div>
         <form class="md:w-2/3 space-x-8 w-[90%] flex flex-row items-center justify-center">
             <div>
                 <label for="lane" class="text-lg">Línea</label>
-                <select class="ml-2 px-8 text-center py-2 rounded-lg" name="lane">
-                <option>R1</option>
-                <option>R2</option>
+                <select class="px-8 py-2 ml-2 text-center rounded-lg" name="lane">
+                    <option>R1</option>
+                    <option>R2</option>
                 </select>
             </div>
-            <?php if(isset($_GET['lane'])) : ?>
-            <div>
-                <label for="direction" class="text-lg">Dirección</label>
-                <select class="ml-2 px-8 text-center py-2 rounded-lg" name="direction">
-                    <option>IDA</option>
-                    <option>VUELTA</option>
-                </select>
-            </div>
+            <?php if (isset($_GET['lane'])) : ?>
+                <div>
+                    <label for="direction" class="text-lg">Dirección</label>
+                    <select class="px-8 py-2 ml-2 text-center rounded-lg" name="direction">
+                        <option>IDA</option>
+                        <option>VUELTA</option>
+                    </select>
+                </div>
             <?php endif; ?>
-            <input type="submit" value="Buscar" class="px-5 py-2 text-white bg-fuchsia-900 rounded-lg font-semibold">
-</form>
+            <input type="submit" value="Buscar" class="px-5 py-2 font-semibold text-white rounded-lg bg-fuchsia-900">
+        </form>
+        <?php if (isset($_GET['direction']) && isset($_GET['lane'])) : ?>
+            <div class="mt-8 w-[90%] flex flex-col justify-start items-center space-y-4">
+                <div class="flex flex-row items-center justify-center w-full space-x-24 text-lg">
+                    <div><b>Origen:</b> Reus</div>
+                    <div><b>Destino:</b> Barcelona-Sants</div>
+                    <div><b>Horarios:</b> 24</div>
+                    <a href="./users/new.php" class="flex items-center justify-between px-3 py-1 font-semibold border border-2 rounded-lg w-fit hover:bg-emerald-500 text-emerald-500 hover:text-white hover:stroke-white stroke-emerald-500 border-emerald-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-5 h-5 mr-2 stroke-inherit">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    Añadir Horario
+                </a>
+                </div>
+                <div class="flex flex-row items-center justify-around w-full p-2 text-xl font-semibold text-center border-b border-b-black">
+                    <div class="w-[20%]">Salida</div>
+                    <div class="w-[20%]">Llegada</div>
+                    <div class="w-[20%]">Paradas</div>
+                    <div class="w-[20%]">Duración</div>
+                    <div class="w-[20%]">Acciones</div>
+                </div>
+                <div class="flex flex-row items-center justify-around w-full text-lg text-center">
+                    <div class="w-[20%]">14:05</div>
+                    <div class="w-[20%]">15:25</div>
+                    <div class="w-[20%]">17</div>
+                    <div class="w-[20%]">1h 20m</div>
+                    <div class="flex flex-row items-center justify-center space-x-2 w-[20%]">
+                        <a href="./users/edit.php?id=1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 stroke-emerald-600">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                            </svg></a>
+                        <a href="./users/delete.php?id=1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 stroke-red-600">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                            </svg></a>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
+    </main>
+</body>
+
+</html>
