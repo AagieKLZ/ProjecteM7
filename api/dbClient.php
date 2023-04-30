43418bc201c392a370b70c1423ebdf021cbdb048
A1bert04 <albertbru04@gmail.com>
2023-04-30 16:15:58 +0200

    Code cleanup

--- api/dbClient.php
+++ api/dbClient.php
@@ -42,7 +42,7 @@ class dbClient
         $this->contrasenya = $password;
     }
 
-    public static function getInstance()
+    public static function getInstance(): dbClient
     {
         if (!isset(self::$instance)) {
             self::$instance = new dbClient();
@@ -57,7 +57,7 @@ class dbClient
      * @param $params array The parameters to bind to the query
      * @return array with the results of the query
      */
-    public function query($sql, $params)
+    public function query($sql, $params): array
     {
         $stmt = $this->conn->prepare($sql);
         $stmt->execute($params);
@@ -80,7 +80,7 @@ class dbClient
      * @return int the last insert id
      * @throws Exception if there is no last insert id
      */
-    public function lastInsertId()
+    public function lastInsertId(): int
     {
         $id = $this->conn->lastInsertId();
         if ($id == 0) {
@@ -99,18 +99,5 @@ class dbClient
         $stmt = $this->conn->prepare($sql);
         $stmt->execute($params);
     }
-
-    /**
-     * Creates the tables if they don't exist
-     * @return void
-     */
-    private function createTables()
-    {
-        // Get the content of createTables.sql
-        $sql = file_get_contents("./api/createTables.sql");
-        // Execute the query
-        $this->conn->exec($sql);
-    }
-
 
 }
