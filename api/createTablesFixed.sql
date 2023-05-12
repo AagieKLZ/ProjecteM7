c7ff11aa9c954de8206a3d85d71043bd527d5736
A1bert04 <albertbru04@gmail.com>
2023-05-12 08:11:15 +0200

    Restructured createTablesFixed.sql

--- api/createTablesFixed.sql
+++ api/createTablesFixed.sql
@@ -4,6 +4,7 @@ CREATE TABLE if not exists stations
     name varchar(50) not null UNIQUE,
     PRIMARY KEY (id)
 );
+
 CREATE TABLE if not exists routes
 (
     id     int         not null auto_increment,
@@ -13,15 +14,6 @@ CREATE TABLE if not exists routes
     INDEX (name)
 );
 
-CREATE TABLE IF NOT EXISTE route_trains
-(
-    route_id VARCHAR(3) NOT NULL,
-    train_num INT NOT NULL,
-    PRIMARY KEY (route_id, train_num),
-    CONSTRAINT fk_route_id FOREIGN KEY (route_id) REFERENCES routes (name) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_train_num FOREIGN KEY (train_num) REFERENCES trains (number) ON DELETE CASCADE ON UPDATE CASCADE
-)
-
 CREATE TABLE if not exists schedules
 (
     station_id  int        not null,
@@ -29,9 +21,18 @@ CREATE TABLE if not exists schedules
     train_num   int        not null,
     stop_number smallint   not null,
     PRIMARY KEY (train_num, stop_number),
-    CONSTRAINT fk_route_id FOREIGN KEY (route_id) REFERENCES routes (name) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_station_id FOREIGN KEY (station_id) REFERENCES stations (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE if not exists route_trains
+(
+    route_id VARCHAR(3) NOT NULL,
+    train_num INT NOT NULL,
+    PRIMARY KEY (route_id, train_num),
+    CONSTRAINT fk_route_id FOREIGN KEY (route_id) REFERENCES routes (name) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_train_num FOREIGN KEY (train_num) REFERENCES schedules (train_num) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 
 CREATE TABLE if not exists users
 (
